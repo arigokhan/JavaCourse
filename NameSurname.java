d45014546e3bb3f31617abc709640bf82eff84b8
Gokhan Ari <arigokhan92@gmail.com>
2021-06-18 13:20:16 +0300

    Lesson 5 Homework - Gokhan Ari

---
+++ NameSurname.java
@@ -0,0 +1,11 @@
+public class NameSurname {
+
+	public static void main (String[] args) {
+	
+		String name = "Gokhan";
+		String surname = "Ari";
+		System.out.print("My name is " + name + ", " + surname);
+	}
+}
+
+
