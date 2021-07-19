720ba43bc176e8d025433d22dfd2eac123fc6a8c
Gokhan Ari <arigokhan92@gmail.com>
2021-07-19 15:16:41 +0300

    Employee form created with create button

--- src/md/tekwill/lesson16/domain/Employee.java
+++ src/md/tekwill/lesson16/domain/Employee.java
@@ -1,21 +1,54 @@
 package md.tekwill.lesson16.domain;
 
+import javax.swing.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import md.tekwill.lesson16.service.impl.EmployeeServiceImpl;
+
 public class Employee {
     private long id;
     private String firstName;
     private String lastName;
     private Department department;
     private static long idCount;
+    //    private JPanel Employee;
+    public JPanel panel1;
+    private JTextField idText;
+    private JTextField firstNameText;
+    private JTextField lastNameText;
+    private JComboBox departmentBox;
+    private JButton createButton;
 
     public Employee(String firstName, String lastName, Department department) {
         id = ++idCount;
+
         this.firstName = firstName;
         this.lastName = lastName;
         this.department = department;
     }
 
     public Employee() {
-        id = ++idCount;
+
+        //id = ++idCount;
+        //idText.setText(""+id);
+        addToJComboBox();
+        createButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                EmployeeServiceImpl employeeService = new EmployeeServiceImpl();
+                Department itDepartment = new Department(departmentBox.getSelectedItem().toString());
+                Employee employee1 = employeeService.create(new Employee(firstNameText.getText(), lastNameText.getText(), itDepartment));
+                Employee savedEmployee = employeeService.create(employee1);
+
+                idText.setText("" + savedEmployee.id);
+
+                System.out.println(employeeService.read(savedEmployee.getId()));
+                JOptionPane.showMessageDialog(null, "Object created succesufully!");
+
+            }
+        });
     }
 
     public void setDepartment(Department department) {
@@ -58,5 +91,14 @@ public class Employee {
                 ", lastName='" + lastName + '\'' +
                 ", department=" + department +
                 '}';
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+
+    public void addToJComboBox() {
+        departmentBox.addItem(("IT"));
+        departmentBox.addItem(("HR"));
     }
 }
--- src/md/tekwill/lesson16/test/TestEmployee.java
+++ src/md/tekwill/lesson16/test/TestEmployee.java
@@ -4,26 +4,35 @@ import md.tekwill.lesson16.domain.Department;
 import md.tekwill.lesson16.domain.Employee;
 import md.tekwill.lesson16.service.impl.EmployeeServiceImpl;
 
+import javax.swing.*;
+
 public class TestEmployee {
     public static void main(String[] args) {
-        EmployeeServiceImpl employeeService= new EmployeeServiceImpl();
-        Department itDepartment = new Department("IT");
-        Employee employee1= new Employee("Ion","Paunescu",itDepartment);
+//        EmployeeServiceImpl employeeService= new EmployeeServiceImpl();
+//        Department itDepartment = new Department("IT");
+//        Employee employee1= new Employee("Ion","Paunescu",itDepartment);
+//
+//
+//        Employee savedEmployee = employeeService.create(employee1);
+//        //employeeService.update(employee1.getId(),employeeToUpdate);
+//        System.out.println(employeeService.read(savedEmployee.getId()));
+//
+//
+//        //UPDATE
+//        Employee updatedEmployee= new Employee("Vasile","Paunescu",itDepartment);
+//        boolean hasBennUpdated = employeeService.update(savedEmployee.getId(),updatedEmployee);
+//        System.out.println(employeeService.read(savedEmployee.getId()) + "has been updated: " + hasBennUpdated);
+//
+//
+//        //DELETE
+//        employeeService.delete(savedEmployee.getId());
 
+        JFrame frame = new JFrame("Employee");
+        frame.setContentPane(new Employee().panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
 
-        Employee savedEmployee = employeeService.create(employee1);
-        //employeeService.update(employee1.getId(),employeeToUpdate);
-        System.out.println(employeeService.read(savedEmployee.getId()));
-
-
-        //UPDATE
-        Employee updatedEmployee= new Employee("Vasile","Paunescu",itDepartment);
-        boolean hasBennUpdated = employeeService.update(savedEmployee.getId(),updatedEmployee);
-        System.out.println(employeeService.read(savedEmployee.getId()) + "has been updated: " + hasBennUpdated);
-
-
-        //DELETE
-        employeeService.delete(savedEmployee.getId());
 
 
     }
